5f0e1e034d8f1355c4eca39a96d9482a252bb6d4
marcos0187 <meduardoc87@LAPTOP-C3.localdomain>
2021-09-30 08:32:10 -0500

    first commit

---
+++ Unit1/VariableDeclarations.js
@@ -0,0 +1,9 @@
+var MyVar;
+let MyLet;
+const MyConst=3.1416;
+
+console.log(`MyVar: ${MyVar}, MyLet: ${MyLet}, MyConst: ${MyConst}, HoistedVariable: ${HoistedVariable}`);
+
+//Hoisting
+
+var HoistedVariable = 25;

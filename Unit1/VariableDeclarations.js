e6b077447ddbb19892cae51661d952ffe46eb689
marcos0187 <meduardoc87@LAPTOP-C3.localdomain>
2021-10-19 12:41:15 -0500

    First code session Variables

--- Unit1/VariableDeclarations.js
+++ Unit1/VariableDeclarations.js
@@ -1,9 +1,18 @@
-var MyVar;
-let MyLet;
-const MyConst=3.1416;
 
-console.log(`MyVar: ${MyVar}, MyLet: ${MyLet}, MyConst: ${MyConst}, HoistedVariable: ${HoistedVariable}`);
+//Global scope
+var myVar = 100;
+let myLet = 15;
+const myConst = 3.1416;
+
+console.log(`myVar: ${myVar}, myLet: ${myLet}, myConst: ${myConst}, HoistedVariable: ${hoistedVariable}`);
 
 //Hoisting
 
-var HoistedVariable = 25; //var hoisted
+var hoistedVariable = 25; //var hoisted
+
+//Local scope
+if (typeof myVar !== "undefined") {//Execute Context
+    console.log(myVar);
+    var myVar = 50;
+    console.log(myVar);
+};
